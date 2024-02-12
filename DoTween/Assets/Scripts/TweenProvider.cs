de0408b410284bb54ba4bf6015754005d613ddd9
风见壬 <ostxen@gmail.com>
2024-02-12 18:17:32 +0800

    DoTween  常用的链式设置和事件响应

--- DoTween/Assets/Scripts/TweenProvider.cs
+++ DoTween/Assets/Scripts/TweenProvider.cs
@@ -9,6 +9,9 @@ public class TweenProvider : MonoBehaviour
 {
     [SerializeField] private Transform _transCube;
 
+    [SerializeField] private Vector3 _rate;
+    [SerializeField] private Vector3 _target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,20 +26,72 @@ public class TweenProvider : MonoBehaviour
 
 
     [Button("使用通用方法改变位置")]
-    private void ChangePos(Vector3 changeRate,Vector3 targetValue)
+    private void ChangePos()
     {
-        DOTween.To(() => _transCube.position, changeRate=> _transCube.position = changeRate, targetValue, 1);
+        DOTween.To(() => _transCube.position, _rate => _transCube.position = _rate, _target, 1);
     }
 
     [Button("使用通用方法改变旋转")]
-    private void ChangeRot(Vector3 changeRate,Vector3 targetValue)
+    private void ChangeRot()
     {
-        DOTween.To(() => _transCube.rotation, changeRate => _transCube.rotation = changeRate, targetValue, 3);
+        DOTween.To(() => _transCube.rotation, _rate => _transCube.rotation = _rate, _target, 3);
     }
 
     [Button("使用通用方法改变缩放")]
-    private void ChangeScale(Vector3 changeRate,Vector3 targetValue)
+    private void ChangeScale()
     {
-        DOTween.To(() => _transCube.localScale, changeRate => _transCube.localScale = changeRate, targetValue, 4);
+        DOTween.To(() => _transCube.localScale, _rate => _transCube.localScale = _rate, _target, 4);
     }
+
+
+    [Button("使用便捷方法设置位置")]
+    private void SetPos()
+    {
+        _transCube.DOMove(Vector3.right * 3, 2);
+    }
+
+    [Button("使用便捷方法设置旋转")]
+    private void SetRot()
+    {
+        _transCube.DORotate(Vector3.right * 100, 2);
+    }
+
+    [Button("使用便捷方法设置缩放")]
+    private void SetScale()
+    {
+        _transCube.DOScale(Vector3.one * 5, 2);
+    }
+
+    [Button("改变位置后循环多次")]
+    private void ChangePosMoreTime()
+    {
+        DOTween.To(() => _transCube.position, _rate => _transCube.position = _rate, _target, 2)
+            .SetLoops(3, LoopType.Restart);
+    }
+
+    [Button("改变缩放后循环多次")]
+    private void ChangeScaleMoreTime()
+    {
+        _transCube.DOScale(Vector3.one * 2, 1).SetTarget(_transCube);
+    }
+
+
+    [Button("其他设置")]
+    private void ChangeOther()
+    {
+        _transCube.DOMove(Vector3.right * 4, 3).SetSpeedBased(false);
+    }
+
+    [Button("动画完成后事件响应")]
+    private void ChangeTransWithEvent()
+    {
+        _transCube.DOMoveX(4, 2).OnStart(OnEvent);
+    }
+
+    private void OnEvent()
+    {
+        Debug.Log("动画补间完成");
+    }
+
 }
+ 
