39f16a6fd5e2113bb9d3cc2ed65c5787f62d7d1a
wucube <ostxen@gmail.com>
2024-02-23 12:50:42 +0800

    UniTask 基础用法——异步操作转为UniTask实现

--- UniTask/Assets/Scripts/TaskProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-
-public class TaskProvider : MonoBehaviour
-{
-    
-}
---
+++ UniTask/Assets/Scripts/UniTaskAsyncSample_Base.cs
@@ -0,0 +1,11 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class UniTaskAsyncSample_Base
+{
+    public async UniTask<Object> LoadAsync<T>(string path) where T : Object
+    {
+        var asyncOperation = Resources.LoadAsync<T>(path);
+        return (await asyncOperation);
+    }
+}
