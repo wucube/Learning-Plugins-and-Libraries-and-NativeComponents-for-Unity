108246eb2d3105a78920c24750ae1ca7df400a5e
风见壬 <ostxen@gmail.com>
2024-02-24 17:31:42 +0800

    UniTask 的 WhenAll 与 WhenAny

---
+++ UniTask/Assets/Scripts/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Runner
+{
+    public Transform Target;
+    public float Speed = 5f;
+    public Vector3 StartPos;
+    
+    /// <summary>
+    /// 达到目标
+    /// </summary>
+    public bool ReachGoal = false;
+
+    public void Reset()
+    {
+        ReachGoal = false;
+        Target.position = StartPos;
+    }
+}
---
+++ UniTask/Assets/Scripts/UniTaskWhenTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UniTaskWhenTest : MonoBehaviour
+{
+    public Runner FirstRunner;
+    public Runner SecondRunner;
+
+    public Button FirstRunButton;
+    public Button SecondRunButton;
+
+    public Button WhenAllButton;
+    public Button WhenAnyButton;
+    public Button ResetButton;
+
+    public float TotalDistance = 15;
+    public Text WinnerText;
+    public Text CompleteText;
+
+    void Start()
+    {
+        FirstRunButton.onClick.AddListener(OnClickFirstRun);
+        SecondRunButton.onClick.AddListener(OnClickSecondRun);
+        
+        WhenAllButton.onClick.AddListener(OnClickWhenAll);
+        WhenAnyButton.onClick.AddListener(OnClickWhenAny);
+        ResetButton.onClick.AddListener(OnClickReset);
+    }
+
+    private void OnClickReset()
+    {
+        FirstRunner.Reset();
+        SecondRunner.Reset();
+        CompleteText.text = string.Empty;
+        WinnerText.text = string.Empty;
+    }
+    
+    private async UniTask RunSomeOne(Runner runner)
+    {
+        runner.Reset();
+        float totalTime = TotalDistance / runner.Speed;
+        //流逝的时间
+        float timeElapsed = 0;
+        
+        while (timeElapsed <= totalTime)
+        {
+            timeElapsed += Time.deltaTime;
+            //等待下一帧（Update之后）执行
+            await UniTask.NextFrame();
+            float runDistance = Mathf.Min(timeElapsed, totalTime) * runner.Speed;
+            runner.Target.position = runner.StartPos + Vector3.right * runDistance;
+        }
+        
+        runner.ReachGoal = true;
+    }
+    
+    private async void OnClickFirstRun()
+    {
+        await RunSomeOne(FirstRunner);
+    }
+
+    private async void OnClickSecondRun()
+    {
+        await RunSomeOne(SecondRunner);
+    }
+
+    private async void OnClickWhenAll()
+    {
+        var firstRunnerReach = UniTask.WaitUntil(() => FirstRunner.ReachGoal);
+        // WaitUntilValueChanged 也很好用
+        var secondRunnerReach = UniTask.WaitUntil(() => SecondRunner.ReachGoal);
+        
+        await UniTask.WhenAll(firstRunnerReach, secondRunnerReach);
+        
+        // 注意，whenAll可以用于平行执行多个资源的读取，非常有用！
+        // var (a, b, c) = await UniTask.WhenAll(
+        //LoadAsSprite("foo"),
+        //LoadAsSprite("bar"),
+        //LoadAsSprite("baz"));
+        
+        CompleteText.text = "双方都抵达了终点，比赛结束";
+    }
+
+    private async void OnClickWhenAny()
+    {
+        var firstRunnerReach = UniTask.WaitUntil(() => FirstRunner.ReachGoal);
+        var secondRunnerReach = UniTask.WaitUntil(() => SecondRunner.ReachGoal);
+
+        await UniTask.WhenAny(firstRunnerReach, secondRunnerReach);
+
+        string winner = FirstRunner.ReachGoal ? "绿色小球" : "黄色小球";
+        WinnerText.text = $"{winner}率先抵达了终点，获得了胜利";
+    }
+    
+    
+}
